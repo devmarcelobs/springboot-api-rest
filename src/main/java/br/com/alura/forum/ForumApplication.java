44c7d17cf5b2f6f10e8c7aa49b8a9933a02a90fe
Marcelo Siqueira <marcelobolsoni@outlook.com>
2022-08-01 11:23:59 -0300

    Fix: simplificando a paginação do TopicosController

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport //habilita o spring pegar as informações de paginação vindos do navegador repassando para o sring data
 public class ForumApplication {
 
 	public static void main(String[] args) {
