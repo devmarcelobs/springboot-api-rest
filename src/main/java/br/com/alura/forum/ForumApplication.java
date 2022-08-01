df79012dde032af037f20a61411d69542ac56b50
Marcelo Siqueira <marcelobolsoni@outlook.com>
2022-08-01 11:52:08 -0300

    Feat: adicionado cache na classe TopicosController

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,10 +2,12 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport //habilita o spring pegar as informações de paginação vindos do navegador repassando para o sring data
+@EnableCaching //habilita o uso de cache na aplicação
 public class ForumApplication {
 
 	public static void main(String[] args) {
