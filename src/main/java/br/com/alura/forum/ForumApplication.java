052814026204217c63ddcffc8e2760436ac9c877
Marcelo Siqueira <marcelobolsoni@outlook.com>
2022-08-03 11:37:04 -0300

    Feat: adicionando o swagger para documentação do projeto

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableSpringDataWebSupport //habilita o spring pegar as informações de paginação vindos do navegador repassando para o sring data
 @EnableCaching //habilita o uso de cache na aplicação
+@EnableSwagger2
 public class ForumApplication {
 
 	public static void main(String[] args) {
